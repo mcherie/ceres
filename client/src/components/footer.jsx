980631161745eddb8be90251d7ca9acfce5d2d3e
Cher <49999513+mcherie@users.noreply.github.com>
2020-09-04 21:18:12 -0400

    updated resume pdf

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { styles } from "../styles"
-import ResumePDF from "../images/cherg_dev.pdf"
+import ResumePDF from "../images/cherdev.pdf"
 
 export const Footer = () => {
   return (
